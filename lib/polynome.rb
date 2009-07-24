518c230a35d75babeba97452d462353b7c0ea849
Sam Aaron <samaaron@gmail.com>
2009-07-24 18:17:42 +0200

    add activesupport to vendor

--- lib/polynome.rb
+++ lib/polynome.rb
@@ -1,3 +1,6 @@
 $:.push File.dirname(__FILE__) + '/../vendor/rosc/lib'
+$:.push File.dirname(__FILE__) + '/../vendor/activesupport/lib'
 
+#require vendored stuff
 require 'osc'
+require 'activesupport'
