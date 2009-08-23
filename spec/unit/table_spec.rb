cd1ab585383c5ceb9232136302307052a8adfd90
Sam Aaron <samaaron@gmail.com>
2009-08-23 22:36:34 +0200

    replace references to rack with table

--- spec/unit/table_spec.rb
+++ spec/unit/table_spec.rb
@@ -7,56 +7,56 @@ describe Polynome::Table do
 
   describe "initialisation" do
     it "should be possible to initialise with default values" do
-      rack = Polynome::Table.new
-      rack.should_not be_nil
-      rack.shutdown
+      table = Polynome::Table.new
+      table.should_not be_nil
+      table.shutdown
     end
 
     it "should be possible to initialise again with default values" do
-      rack = Polynome::Table.new
-      rack.should_not be_nil
-      rack.shutdown
+      table = Polynome::Table.new
+      table.should_not be_nil
+      table.shutdown
     end
 
     it "should be possible to specify the in port to use" do
-      rack = Polynome::Table.new(:in_port => 1234)
-      rack.in_port.should == 1234
-      rack.shutdown
+      table = Polynome::Table.new(:in_port => 1234)
+      table.in_port.should == 1234
+      table.shutdown
     end
 
     it "should be possible to specify the out port to use" do
-      rack = Polynome::Table.new(:out_port => 5678)
-      rack.out_port.should == 5678
-      rack.shutdown
+      table = Polynome::Table.new(:out_port => 5678)
+      table.out_port.should == 5678
+      table.shutdown
     end
 
     it "should be possible to specify the out host to use" do
-      rack = Polynome::Table.new(:out_host => 'beans.com')
-      rack.out_host.should == 'beans.com'
-      rack.shutdown
+      table = Polynome::Table.new(:out_host => 'beans.com')
+      table.out_host.should == 'beans.com'
+      table.shutdown
     end
   end
 
   describe "Adding a Virtual Monome" do
     before(:each) do
-      @rack     = Polynome::Table.new(:in_port => 4443)
+      @table     = Polynome::Table.new(:in_port => 4443)
       @receiver = Tosca::Receiver.new(5544)
       @sender   = Tosca::Sender.new(4433)
-      @rack.boot
+      @table.boot
     end
 
     after(:each) do
-      @rack.shutdown
+      @table.shutdown
     end
 
-    it "should know that no vms are currently in a new rack" do
-      @rack.num_vms.should == 0
+    it "should know that no vms are currently in a new table" do
+      @table.num_vms.should == 0
     end
 
     it "should know that there is one vm after adding one" do
       vm = Polynome::VirtualMonome.new
-      @rack.add_vm(vm)
-      @rack.num_vms.should == 1
+      @table.add_vm(vm)
+      @table.num_vms.should == 1
       vm.power_down
     end
   end
@@ -65,12 +65,12 @@ describe Polynome::Table do
     before(:each) do
       @sender   = Tosca::Sender.new(4433)
       @receiver = Tosca::Receiver.new(5544)
-      @rack     = Polynome::Table.new(:in_port => 4433)
-      @rack.boot
+      @table     = Polynome::Table.new(:in_port => 4433)
+      @table.boot
     end
 
     after(:each) do
-      @rack.shutdown
+      @table.shutdown
     end
 
     it "should be possible to register with the Table to receive all output" do
