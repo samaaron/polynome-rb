41d26f69e7aadaee529d1c528346ab045c3faafc
Sam Aaron <samaaron@gmail.com>
2009-08-26 12:11:13 +0200

    extend ThreadedLogger to allow for multiple (named) logs

--- vendor/threaded_logger/lib/threaded_logger.rb
+++ vendor/threaded_logger/lib/threaded_logger.rb
@@ -3,44 +3,31 @@ class ThreadedLogger
     initialize
   end
 
-  def self.log(message)
-    @semaphore.synchronize do
-      @messages << message
-      @num_messages_received += 1
-    end
-  end
-
-  def self.length
-    @semaphore.synchronize do
-      @messages.length
-    end
-  end
-
-  def self.outstream=(outstream=STDOUT)
-    @semaphore.synchronize do
-      @outstream = outstream
-    end
-  end
-
-  def self.outstream
-    @semaphore.synchronize do
-      @outstream
-    end
-  end
-
-  def self.num_messages_received
-    @semaphore.synchronize do
-      @num_messages
-    end
-  end
-
-  def self.num_messages_logged
-    @semaphore.synchronize do
-      @num_messages_logged
-    end
-  end
-
   def self.initialize
+    @logs = {}
+  end
+
+  #make sure ThreadedLogger is initialized
+  initialize
+
+  def self.logs
+    @logs.values
+  end
+
+  def self.create_log(name)
+    log = new(name)
+    @logs[name.to_s] = log
+  end
+
+  def self.get_log(name)
+    @logs[name.to_s]
+  end
+
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name.to_s
     @messages = Queue.new
     @outstream = STDOUT
     @semaphore = Mutex.new
@@ -48,7 +35,44 @@ class ThreadedLogger
     @num_messages_logged = 0
   end
 
-  def self.start
+  def log(message)
+    @semaphore.synchronize do
+      @messages << message
+      @num_messages_received += 1
+    end
+  end
+
+  def length
+    @semaphore.synchronize do
+      @messages.length
+    end
+  end
+
+  def outstream=(outstream=STDOUT)
+    @semaphore.synchronize do
+      @outstream = outstream
+    end
+  end
+
+  def outstream
+    @semaphore.synchronize do
+      @outstream
+    end
+  end
+
+  def num_messages_received
+    @semaphore.synchronize do
+      @num_messages
+    end
+  end
+
+  def num_messages_logged
+    @semaphore.synchronize do
+      @num_messages_logged
+    end
+  end
+
+  def start
     @thread = Thread.new do
       loop do
         @outstream << "#{@messages.pop}\n"
@@ -57,9 +81,8 @@ class ThreadedLogger
     end
   end
 
-  def self.stop
+  def stop
     @thread.kill
   end
 
-  initialize
 end
--- vendor/threaded_logger/spec/unit/threaded_logger_spec.rb
+++ vendor/threaded_logger/spec/unit/threaded_logger_spec.rb
@@ -9,92 +9,123 @@ describe ThreadedLogger do
     ThreadedLogger.should_not be_nil
   end
 
-  it "should start out with no messages inside it" do
-    ThreadedLogger.length.should == 0
+  it "should start out with no logs" do
+    ThreadedLogger.logs.size.should == 0
   end
 
-  it "should be possible to send it a message" do
-    ThreadedLogger.log "hello there"
-    ThreadedLogger.length.should == 1
+  it "should be possible to create a new log with a symbol" do
+    ThreadedLogger.create_log(:benny)
+    ThreadedLogger.logs.size.should == 1
   end
 
-  it "should be possible to sent it multiple messages" do
-    ThreadedLogger.log "hello"
-    ThreadedLogger.log "there"
-    ThreadedLogger.log "world"
-    ThreadedLogger.length.should == 3
+  it "should be possible to create a new log with a string" do
+    ThreadedLogger.create_log("toddy")
+    ThreadedLogger.logs.size.should == 1
   end
 
-  it "should use STDOUT as the default outstream" do
-    ThreadedLogger.outstream.should == STDOUT
+  it "should be possible to create multiple logs" do
+    ThreadedLogger.create_log(:boris)
+    ThreadedLogger.create_log(:beans)
+    logs = ThreadedLogger.logs
+    logs.size.should == 2
+    logs.each{|log| log.class.should == ThreadedLogger}
   end
 
-  it "should be possible to initialize one with an outstream" do
-    outstream = ""
-    ThreadedLogger.outstream = outstream
-    ThreadedLogger.outstream.should == outstream
-  end
-
-  describe "with starting and stopping and with a given outstream" do
+  describe "with a new log" do
     before(:each) do
-      @outstream = ""
-      ThreadedLogger.outstream = @outstream
-      ThreadedLogger.start
+      ThreadedLogger.create_log(:ted)
+      @log = ThreadedLogger.get_log(:ted)
     end
 
-    after(:each) do
-      ThreadedLogger.stop
+    it "should have the right name" do
+      @log.name.should == "ted"
     end
 
-
-    it "should start with the outstream being an empty string" do
-      @outstream.should == ""
+    it "should start out with no messages inside it" do
+      @log.length.should == 0
     end
 
-    it "should have the correct outstream" do
-      ThreadedLogger.outstream.should == @outstream
+    it "should be possible to send it a message" do
+      @log.log "hello there"
+      @log.length.should == 1
     end
 
-    it "should place the messages to the outstream" do
-      time = Time.now
+    it "should be possible to sent it multiple messages" do
+      @log.log "hello"
+      @log.log "there"
+      @log.log "world"
+      @log.length.should == 3
+    end
 
-      ThreadedLogger.log "hi"
-      ThreadedLogger.log "there"
-      ThreadedLogger.log "how"
-      ThreadedLogger.log "are"
-      ThreadedLogger.log "you?"
+    it "should use STDOUT as the default outstream" do
+      @log.outstream.should == STDOUT
+    end
 
-      while(ThreadedLogger.num_messages_logged < 5) do
-        if Time.now - time > 0.5
-          raise "Taking too long to place messages onto the ThreadedLogger outstream, only managed to receive #{ThreadedLogger.num_messages} messages"
-        end
+    it "should be possible to initialize one with an outstream" do
+      outstream = ""
+      @log.outstream = outstream
+      @log.outstream.should == outstream
+    end
+
+    describe "with starting and stopping and with a given outstream" do
+      before(:each) do
+        @outstream = ""
+        @log.outstream = @outstream
+        @log.start
       end
 
-      @outstream.should == "hi\nthere\nhow\nare\nyou?\n"
-    end
+      after(:each) do
+        @log.stop
+      end
 
-    it "should be possible to place 10000 messages to the outstream from 1000 different threads" do
-      time = Time.now
+      it "should start with the outstream being an empty string" do
+        @outstream.should == ""
+      end
 
-      1000.times do |i|
-        thread_name = "thread #{i}"
+      it "should have the correct outstream" do
+        @log.outstream.should == @outstream
+      end
 
-        Thread.new do
-          10.times do |j|
-            message = "#{thread_name}: message #{j}"
-            ThreadedLogger.log message
+      it "should place the messages to the outstream" do
+        time = Time.now
+
+        @log.log "hi"
+        @log.log "there"
+        @log.log "how"
+        @log.log "are"
+        @log.log "you?"
+
+        while(@log.num_messages_logged < 5) do
+          if Time.now - time > 0.5
+            raise "Taking too long to place messages onto the @log outstream, only managed to receive #{@log.num_messages} messages"
           end
         end
+
+        @outstream.should == "hi\nthere\nhow\nare\nyou?\n"
       end
 
-       while(ThreadedLogger.num_messages_logged < 10000) do
-        if Time.now - time > 3
-          raise "Taking too long to place messages onto the ThreadedLogger outstream, only managed to receive #{ThreadedLogger.num_messages_logged} messages"
-        end
-       end
+      it "should be possible to place 10000 messages to the outstream from 1000 different threads" do
+        time = Time.now
 
-      @outstream.split("\n").size.should == 10000
+        1000.times do |i|
+          thread_name = "thread #{i}"
+
+          Thread.new do
+            10.times do |j|
+              message = "#{thread_name}: message #{j}"
+              @log.log message
+            end
+          end
+        end
+
+        while(@log.num_messages_logged < 10000) do
+          if Time.now - time > 3
+            raise "Taking too long to place messages onto the @log outstream, only managed to receive #{@log.num_messages_logged} messages"
+          end
+        end
+
+        @outstream.split("\n").size.should == 10000
+      end
     end
   end
 end
-
