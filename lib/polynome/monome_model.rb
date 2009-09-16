317719dddcf82b2258dec24dc7e707a5dde09179
Sam Aaron <samaaron@gmail.com>
2009-09-16 09:32:21 +0200

    add Polynome::MonomeModel

---
+++ lib/polynome/monome_model.rb
@@ -0,0 +1,68 @@
+module Polynome
+  class MonomeModel
+    def self.get_model(model)
+      case model
+      when "40h" then return FourtyH.new
+      when "64"  then return SixtyFour.new
+      when "128" then return OneTwentyEight.new
+      when "256" then return TwoFiftySix.new
+      end
+    end
+  end
+
+  class FourtyH < MonomeModel
+    def width
+      8
+    end
+
+    def height
+      8
+    end
+
+    def protocol
+      "40h"
+    end
+  end
+
+  class SixtyFour < MonomeModel
+    def width
+      8
+    end
+
+    def height
+      8
+    end
+
+    def protocol
+      "series"
+    end
+  end
+
+  class OneTwentyEight < MonomeModel
+    def width
+      16
+    end
+
+    def height
+      8
+    end
+
+    def protocol
+      "series"
+    end
+  end
+
+  class TwoFiftySix < MonomeModel
+    def width
+      16
+    end
+
+    def height
+      16
+    end
+
+    def protocol
+      "series"
+    end
+  end
+end
---
+++ spec/unit/monome_model_spec.rb
@@ -0,0 +1,76 @@
+require File.dirname(__FILE__) + '/../spec_helper.rb'
+include Polynome
+
+describe MonomeModel do
+  describe "get model 40h" do
+    before(:each) do
+      @model = MonomeModel.get_model("40h")
+    end
+
+    it "should have a width of 8" do
+      @model.width.should == 8
+    end
+
+    it "should have a height of 8" do
+      @model.height.should == 8
+    end
+
+    it "should use the 40h protocol" do
+      @model.protocol.should == "40h"
+    end
+  end
+
+  describe "get model 64" do
+    before(:each) do
+      @model = MonomeModel.get_model("64")
+    end
+
+    it "should have a width of 8" do
+      @model.width.should == 8
+    end
+
+    it "should have a height of 8" do
+      @model.height.should == 8
+    end
+
+    it "should use the series protocol" do
+      @model.protocol.should == "series"
+    end
+  end
+
+  describe "get model 128" do
+    before(:each) do
+      @model = MonomeModel.get_model("128")
+    end
+
+    it "should have a width of 16" do
+      @model.width.should == 16
+    end
+
+    it "should have a height of 8" do
+      @model.height.should == 8
+    end
+
+    it "should use the series protocol" do
+      @model.protocol.should == "series"
+    end
+  end
+
+  describe "get model 256" do
+    before(:each) do
+      @model = MonomeModel.get_model("256")
+    end
+
+    it "should have a width of 16" do
+      @model.width.should == 16
+    end
+
+    it "should have a height of 16" do
+      @model.height.should == 16
+    end
+
+    it "should use the series protocol" do
+      @model.protocol.should == "series"
+    end
+  end
+end
