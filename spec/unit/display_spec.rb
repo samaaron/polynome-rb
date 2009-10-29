a164d68e1090d994338ce5e39f6c1b09cc0e2c14
Sam Aaron <samaaron@gmail.com>
2009-10-29 09:05:50 +0100

    remove display abstraction as it overcomplicates matters right now. (Perhaps in the future we might want to separate the monome into a communicator and display)

--- spec/unit/display_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper.rb'
-include Polynome
-
-describe Display do
-  it "should resolve to the correct constant from this context" do
-    Display.should == Polynome::Display
-  end
-end
