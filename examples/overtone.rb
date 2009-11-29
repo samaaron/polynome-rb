2e8bff120266163daf02773d6b7cfdb38a12c4ff
Jeff Rose <rosejn@gmail.com>
2009-11-29 18:17:47 +0100

    Getting it working in linux.

--- examples/overtone.rb
+++ examples/overtone.rb
@@ -10,10 +10,11 @@ class OvertoneController
   include Polynome
 
   def initialize
-    monome_io_file = "/dev/tty.usbserial-m256-203"
+    monome_io_file = "/dev/ttyUSB0"
     monome_model   = "256"
 
     @monome = MonomeSerial::MonomeCommunicator.new(monome_io_file)
+    @synth = OSCSender.new(1234)
   end
 
   def start
@@ -21,6 +22,7 @@ class OvertoneController
       loop do
         action, x, y = @monome.read
         puts [action, x, y].inspect
+        @synth.send("/hit", x, y) if action == :keydown
       end
     end
   end
